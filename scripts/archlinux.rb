d0cece3c8674b348f167a7ea64b0fe195593c968
Ye Shu <chshu@protonmail.ch>
2019-08-24 23:49:46 -0700

    Style fix
    - Fix style according to rubocop feedbacks
    - Customize rubocop configurations
    - Add placeholder script to prevent job `shellcheck` from failing

--- scripts/archlinux.rb
+++ scripts/archlinux.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 class Archlinux
   def self.configure(config, settings)
-
     ENV['VAGRANT_DEFAULT_PROVIDER'] = settings['provider'] ||= 'virtualbox'
 
     # Configure Local Variable To Access Scripts From Remote Location
-    script_dir = File.dirname(__FILE__)
+    # script_dir = File.dirname(__FILE__)
 
     # Configure the Box
     config.vm.box = settings['box'] ||= 'archlinux/archlinux'
@@ -15,13 +16,14 @@ class Archlinux
     # Configure Private Network
     config.vm.hostname = settings['hostname'] ||= 'archlinux'
     if settings['ip'] != 'dhcp'
-      config.vm.network "private_network", ip: settings['ip'] ||= '192.168.10.25'
+      config.vm.network 'private_network',
+        ip: settings['ip'] ||= '192.168.10.25'
     else
-      config.vm.network "private_network", type: "dhcp"
+      config.vm.network 'private_network', type: 'dhcp'
     end
 
     # Configure Additional Network Settings (defined in config.yaml)
-    if settings.has_key?('networks')
+    if settings.key?('networks')
       settings['networks'].each do |net|
         config.vm.network net['type'],
           ip: net['ip'],
@@ -38,24 +40,25 @@ class Archlinux
 
       # Enable DNS proxy in NAT mode
       v.customize ['modifyvm', :id, '--natdnsproxy1', 'on']
-      v.customize ['modifyvm', :id, '--natdnshostresolver1', settings['natdnshostresolver'] ||= 'on']
+      v.customize ['modifyvm',
+                   :id,
+                   '--natdnshostresolver1',
+                   settings['natdnshostresolver'] ||= 'on']
 
       # OS Type
       v.customize ['modifyvm', :id, '--ostype', 'ArchLinux_64']
 
       # Headless vs GUI
-      if settings.has_key?('gui') && settings['gui']
-        v.gui = true
-      end
+      v.gui = true if settings.key?('gui') && settings['gui']
     end
 
     # Override Default SSH port
-    if settings.has_key?('default_ssh_port')
-      config.vm.network "forwarded_port",
+    if settings.key?('default_ssh_port')
+      config.vm.network 'forwarded_port',
         guest: 22,
         host: settings['default_ssh_port'],
         auto_correct: false,
-        id: "ssh"
+        id: 'ssh'
     end
 
     # Default Port Forwarding
@@ -71,20 +74,20 @@ class Archlinux
     }
 
     # Use Default Port Forwarding Unless Stated Otherwise
-    unless settings.has_key?('default_ports') && settings['default_ports'] == false
+    unless settings.key?('default_ports') && settings['default_ports'] == false
       default_ports.each do |guest, host|
-        # if custom port settings does not override default
-        unless settings['ports'].any? { |mapping| mapping['guest'] == guest }
-          config.vm.network 'forwarded_port',
-            guest: guest,
-            host: host,
-            auto_correct: true
-        end
+        # if custom port settings override default
+        next if settings['ports'].any? { |mapping| mapping['guest'] == guest }
+
+        config.vm.network 'forwarded_port',
+          guest: guest,
+          host: host,
+          auto_correct: true
       end
     end
 
     # Add Custom Ports From Configuration
-    if settings.has_key?('ports')
+    if settings.key?('ports')
       settings['ports'].each do |port|
         config.vm.network 'forwarded_port',
           guest: port['guest'],
@@ -93,7 +96,6 @@ class Archlinux
           auto_correct: true
       end
     end
-
   end
 
   def self.backup_mysql(database, dir, config)
