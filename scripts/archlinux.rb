7f3dd30f2cc5b19b0472e7fcff3ba3b59bb7460b
Ye Shu <chshu@protonmail.ch>
2019-09-06 16:27:27 +0800

    Add folder sharing

--- scripts/archlinux.rb
+++ scripts/archlinux.rb
@@ -131,6 +131,60 @@ class Archlinux
         end
       end
     end
+
+    # Copy User Files Over to VM
+    if settings.include? 'copy'
+      settings['copy'].each do |file|
+        config.vm.provision 'file' do |f|
+          f.source = File.expand_path(file['from'])
+          f.destination = file['to'].chomp('/') + '/' + file['from'].split('/').last
+        end
+      end
+    end
+
+    # Register All Of The Configured Shared Folders
+    if settings.include? 'folders'
+      settings['folders'].each do |folder|
+        if File.exist? File.expand_path(folder['map'])
+          mount_options = []
+
+          if folder['type'] == 'nfs'
+            # Available on *nix & Mac OS Host
+            # Check https://www.vagrantup.com/docs/synced-folders/nfs.html
+            mount_options = folder['mount_options'] ? folder['mount_options'] : ['actimeo=1', 'nolock']
+          elsif folder['type'] == 'smb'
+            # Available on Windows & Mac OS Host
+            # Check https://www.vagrantup.com/docs/synced-folders/smb.html
+            mount_options = folder['mount_options'] ? folder['mount_options'] : ['vers=3.02', 'mfsymlinks']
+
+            smb_creds = {
+              smb_host: folder['smb_host'],
+              smb_username: folder['smb_username'],
+              smb_password: folder['smb_password']
+            }
+          end
+
+          # merge with options
+          options = (folder['options'] || {}).merge({ mount_options: mount_options }).merge(smb_creds || {})
+
+          # Double-splat (**) operator only works with symbol keys, so convert
+          options.keys.each{|k| options[k.to_sym] = options.delete(k) }
+
+          config.vm.synced_folder folder['map'], folder['to'],
+            type: folder['type'] ||= nil, # Defaults to Virtualbox if using
+            **options
+
+          # Bindfs support to fix shared folder (NFS) permission issue on Mac
+          if folder['type'] == 'nfs' && Vagrant.has_plugin?('vagrant-bindfs')
+            config.bindfs.bind_folder folder['to'], folder['to']
+          end
+        else
+          config.vm.provision 'shell' do |s|
+            s.inline = '>&2 echo \"Unable to mount one of your folders. Please check your folders in configuration files\"'
+          end
+        end
+      end
+    end
   end
 
   def self.backup_mysql(database, dir, config)
